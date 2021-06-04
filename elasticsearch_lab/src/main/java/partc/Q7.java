becf916357c22382f018209709f702f7f94e870d
Arnald Gabarrell <69221572+Arnaldgg7@users.noreply.github.com>
2021-06-04 23:05:16 +0200

    Add files via upload

---
+++ elasticsearch_lab/src/main/java/partc/Q7.java
@@ -0,0 +1,64 @@
+package partc;
+
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.BucketOrder;
+import org.elasticsearch.search.aggregations.bucket.MultiBucketsAggregation;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
+import util.Utils;
+
+import java.util.Collection;
+
+public class Q7 {
+
+	public static void execute() throws Exception {
+		TransportClient client = Utils.getClient();
+
+		// Build query and execute it
+		SearchResponse response = client.prepareSearch("shakespeare")
+				.setSearchType(SearchType.QUERY_THEN_FETCH)
+				.setScroll(new TimeValue(60000))
+				.setQuery(QueryBuilders.matchQuery("text_entry","love"))
+				.addAggregation(AggregationBuilders.terms("agg1").field("speaker").order(BucketOrder.count(false)).size(Utils.MAX_RESULTS))
+				.setSize(Utils.MAX_RESULTS)
+				.setExplain(true)
+				.get();
+
+
+		Terms agg1 = response.getAggregations().get("agg1");
+
+		SearchHits hits = response.getHits();
+		int hitsCount = hits.getHits().length;
+
+		// We create a do-while loop in order to check that, in case that we would get more than
+		// 10.000 results, the program would be able to scroll through the next page of results
+		// and return all of them.
+		do {
+			// Iterate through query results and print them
+			System.out.println("Query executed successfully. "
+					+ "\nQuery Time: " + response.getTook() + " ms"
+					+ "\nResults Count: " + hitsCount
+					+ "\nResults: ");
+
+			Collection<Terms.Bucket> buckets = (Collection<Terms.Bucket>) agg1.getBuckets();
+
+			for (MultiBucketsAggregation.Bucket bucket : buckets) {
+				System.out.println(bucket.getKeyAsString() +" ("+bucket.getDocCount()+")");
+			}
+
+			response = client.prepareSearchScroll(response.getScrollId())
+					.setScroll(new TimeValue(60000)).execute().actionGet();
+
+			hits = response.getHits();
+			hitsCount = hits.getHits().length;
+		} while (hitsCount > 0);
+
+		Utils.closeClient(client);
+	}
+
+}
